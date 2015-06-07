65695fca0ec735a8c073aaf611b73eccd920c493
Raynold Ng <rayliverpoolrules@gmail.com>
2015-06-07 13:45:10 +0800

    More changes to camp and tank detail

--- src/GameLevels/GameLevel.java
+++ src/GameLevels/GameLevel.java
@@ -24,7 +24,8 @@ import GameLevels.Elements.Player.TankOptions;
 import Managers.GameManager;
 import Managers.ResourceManager;
 
-public class GameLevel extends ManagedGameScene implements IOnSceneTouchListener {
+public class GameLevel extends ManagedGameScene implements
+		IOnSceneTouchListener {
 
 	// ==========================================
 	// CONSTANTS
@@ -35,7 +36,8 @@ public class GameLevel extends ManagedGameScene implements IOnSceneTouchListener
 	// ==========================================
 	public final LevelDef mLevelDef;
 
-	public volatile List<Camp> mCamps = Collections.synchronizedList(new ArrayList<Camp>());
+	public volatile List<Camp> mCamps = Collections
+			.synchronizedList(new ArrayList<Camp>());
 	public volatile List<Edge> mEdges;
 
 	public LinkedList<Player> mPlayers;
@@ -49,72 +51,85 @@ public class GameLevel extends ManagedGameScene implements IOnSceneTouchListener
 	// ==========================================
 
 	// ==========================================
-	// CONSTRUCTOR 
+	// CONSTRUCTOR
 	// ==========================================
 	public GameLevel(final LevelDef pLevelDef) {
 		this.mLevelDef = pLevelDef;
-		
-		//mCamps = Collections.synchronizedList(new ArrayList<Camp>());
+
+		// mCamps = Collections.synchronizedList(new ArrayList<Camp>());
 		mEdges = Collections.synchronizedList(new ArrayList<Edge>());
 	}
 
 	protected void addEdge(EdgeDef curEdge) {
-		// helper method needed as we need to reference the two camps at the two ends
+		// helper method needed as we need to reference the two camps at the two
+		// ends
 		// of edge
 		int v = curEdge.v;
 		int w = curEdge.w;
-		
-		assert v < mCamps.size() && w < mCamps.size(): "v:" + v + " w:" + w + " nCamps: " + mCamps.size();
-		assert v != w : "Edge must point to two distinct camps!";
-		
+
+		// assert v < mCamps.size() && w < mCamps.size(): "v:" + v + " w:" + w +
+		// " nCamps: " + mCamps.size();
+		// assert v != w : "Edge must point to two distinct camps!";
+
 		Edge edge = new Edge(mCamps.get(v), mCamps.get(w), this);
 		mCamps.get(v).addEdge(edge);
 		mCamps.get(w).addEdge(edge);
-		
+
 		mEdges.add(edge);
 	}
-	
+
 	@Override
 	public void onLoadLevel() {
 		GameManager.setGameLevel(this);
-		
+
 		this.addLoadingStep(new LoadingRunnable("Loading game textures", this) {
-			
+
 			@Override
 			public void onLoad() {
-				
-				// set the zoom factor, dependent on map size and the camera dimension
+
+				// set the zoom factor, dependent on map size and the camera
+				// dimension
 				SmoothCamera cam = (SmoothCamera) ResourceManager.getCamera();
 				cam.setZoomFactorDirect(cam.getCameraSceneWidth() / 800);
 				cam.setCenterDirect(400, 240);
-				
+
 				// load camps
-				for(final CampDef curCamp : GameLevel.this.mLevelDef.mCamps) {
-					mCamps.add(new Camp(curCamp.mX, curCamp.mY, curCamp.mSize, curCamp.color, GameLevel.this));
-					//mCamps.add(camp);
+				for (final CampDef curCamp : GameLevel.this.mLevelDef.mCamps) {
+					mCamps.add(new Camp(curCamp.mX, curCamp.mY, curCamp.mSize,
+							curCamp.color, GameLevel.this));
 				}
-				
+
 				// add edges
-				for(final EdgeDef curEdge : GameLevel.this.mLevelDef.mEdges)
+				for (final EdgeDef curEdge : GameLevel.this.mLevelDef.mEdges)
 					GameLevel.this.addEdge(curEdge);
-				
-				sortChildren();	// so that the camps appear above the lines (edges)
+
 			}
 		});
+
+		sortChildren(); // so that the camps appear above the lines
+						// (edges)
+		GameLevel.this.setBackgroundEnabled(true);
+		GameLevel.this.setBackground(new Background(Color.WHITE));
 		
-		this.setBackgroundEnabled(true);
-		this.setBackground(new Background(Color.WHITE));
 		this.setOnSceneTouchListener(this);
-		
+
+	}
+
+	@Override
+	public void onShowScene() {
+
 		// TEST sending a tank detail
-		
+
 		// set up create players, to be done in a more proper manner
 		Player P1 = new Player(1, CampColor.BLUE, TankOptions.E100);
 		Debug.i(TAG, "Number of camps:" + mCamps.size());
-		
-		// END TEST sending a tank detail
-	}
+		Camp test1 = mCamps.get(0);
+		test1.setmPlayer(P1);
+		test1.sendTankDetail(20, test1.getmIncidentEdges().get(0));
 
+		// END TEST sending a tank detail
+
+	}
 
 	@Override
 	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
